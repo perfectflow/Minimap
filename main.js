b6da65b9e4494fd5634990d717a3bacdd8c106cf
Mighty Alex <perfectflow@live.com>
2021-01-01 22:18:47 +0100

    Remove unused code for creating ghost artboard renditions

--- main.js
+++ main.js
@@ -151,15 +151,6 @@ function generateMinimap(){
                 ghostCenter.style.top = panel.offsetHeight - (viewport.bounds.y * viewport.zoomFactor) - panel.offsetHeight - 88;
             // }
         }
-        // const renditionsFiles = await createRenditions();
-        // for(var i = 0; i < renditionsFiles.length; i++){
-        //     const arrayBuffer = await renditionsFiles[i].read({ format: fs.formats.binary });
-        //     const base64 = base64ArrayBuffer(arrayBuffer);
-        //     const image = document.createElement("img");
-        //     image.setAttribute("src", `data:image/png;base64,${base64}`);
-        //     const ghostArtboard = document.getElementById("ghostArtboard-"+i);
-        //     ghostArtboard.appendChild(image);
-        // }
     }else{
         let ghostCenter = document.getElementById("ghostCenter");
         while(ghostCenter.firstChild) {
@@ -179,77 +170,6 @@ async function initializeDefaultSettings(){
             window.localStorage.setItem(key, default_settings[key]);
         }
     }
-}
-
-async function createRenditions() {
-    const folder = await fs.localFileSystem.getTemporaryFolder();
-    let arr = [];
-    for(var i = 0; i < scenegraph.root.children.length; i++){
-        await folder.createFile(`${scenegraph.root.children.at(i).guid}.png`, { overwrite: true }).then(file => {
-            let obj = {};
-            obj.node = scenegraph.root.children.at(i);
-            obj.outputFile = file;
-            obj.type = "png";
-            obj.background = new Color("#ffffff", 0);
-            obj.scale = 1;
-            arr.push(obj);
-        });
-    }
-    const renditionResults = await application.createRenditions(arr);
-    const renditionsFiles = renditionResults.map(a => a.outputFile);
-    return renditionsFiles;
-}
-
-function base64ArrayBuffer(arrayBuffer) {
-    let base64 = ''
-    const encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-
-    const bytes = new Uint8Array(arrayBuffer)
-    const byteLength = bytes.byteLength
-    const byteRemainder = byteLength % 3
-    const mainLength = byteLength - byteRemainder
-
-    let a, b, c, d
-    let chunk
-
-    // Main loop deals with bytes in chunks of 3
-    for (var i = 0; i < mainLength; i = i + 3) {
-        // Combine the three bytes into a single integer
-        chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
-
-        // Use bitmasks to extract 6-bit segments from the triplet
-        a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
-        b = (chunk & 258048) >> 12 // 258048   = (2^6 - 1) << 12
-        c = (chunk & 4032) >> 6 // 4032     = (2^6 - 1) << 6
-        d = chunk & 63               // 63       = 2^6 - 1
-
-        // Convert the raw binary segments to the appropriate ASCII encoding
-        base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
-    }
-
-    // Deal with the remaining bytes and padding
-    if (byteRemainder == 1) {
-        chunk = bytes[mainLength]
-
-        a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
-
-        // Set the 4 least significant bits to zero
-        b = (chunk & 3) << 4 // 3   = 2^2 - 1
-
-        base64 += encodings[a] + encodings[b] + '=='
-    } else if (byteRemainder == 2) {
-        chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
-
-        a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
-        b = (chunk & 1008) >> 4 // 1008  = (2^6 - 1) << 4
-
-        // Set the 2 least significant bits to zero
-        c = (chunk & 15) << 2 // 15    = 2^4 - 1
-
-        base64 += encodings[a] + encodings[b] + encodings[c] + '='
-    }
-
-    return base64
 }
 
 async function show(event) {
