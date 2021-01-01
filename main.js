7e25416ed0eaf7f741ae593f21560fa0861d7ae0
Mighty Alex <perfectflow@live.com>
2021-01-01 20:17:38 +0100

    Improvements to the ghost panel and unused createRenditions function

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-// const application = require("application");
-// const fs = require('uxp').storage;
-const viewport = require("viewport");
-const scenegraph = require("scenegraph");
-const { Artboard } = require("scenegraph");
+const application = require("application");
 const { editDocument } = require("application");
+const viewport = require("viewport");
+const { selection, Artboard, Color } = require("scenegraph");
+let scenegraph = require("scenegraph");
+const fs = require('uxp').storage;
+
 let panel;
 let rootWidth = 50000;
 let panelWidth = 0;
@@ -29,13 +30,14 @@ function create() {
             #settings {width: 100%; display: block;}
             #settings li {width: 100%; display: flex; flex-direction: row; justify-content: space-between;}
             #settings li h3 {margin: 0;}
-            .dark_mode #preview {background: #3C3D3D;}
+            .dark_mode #preview {background: rgba(15, 15, 15, 0.75);}
             .dark_mode #miniViewport {border: 2px solid #FD7422!important;}
             .dark_mode .miniArtboard {border: none!important;}
             .dark_mode #horizontalPointer, .dark_mode #verticalPointer {background: #303031!important;}
-            #ghost {width: 100%; height: 100%; position: fixed; left: 0; top: 0; overflow: hidden; z-index: -1;}
+            #ghost {position: fixed; left: -12px; right: -12px; top: -12px; bottom: -12px; overflow: hidden; z-index: -1;}
             #ghost #ghostCenter {width: 0px; height: 0px; position: absolute; left: 50%; top: 50%;}
-            #ghost #ghostCenter .ghostArtboard {position: absolute; border: 1px solid #CDCDCD; background: #fff;}
+            #ghost #ghostCenter .ghostArtboard {position: absolute; border: 1px solid #CDCDCD; background: #fff; overflow: hidden;}
+            #ghost #ghostCenter .ghostArtboard img {width: 100%;}
         </style>
         <div id="main">
             <div id="preview" title="Click to jump into specific region">
@@ -130,26 +132,36 @@ function generateMinimap(){
 
     if(window.localStorage.getItem("ghost_mode") == "true"){
         let ghostCenter = document.getElementById("ghostCenter");
-        while (ghostCenter.firstChild) {
+        while(ghostCenter.firstChild) {
             ghostCenter.removeChild(ghostCenter.firstChild);
         }
         for (let i = 0; i < scenegraph.root.children.length; i++) {
             let item = scenegraph.root.children.at(i);
-            if (item instanceof Artboard) {
+            // if (item instanceof Artboard) {
                 let ghostArtboard = document.createElement("div");
                 ghostArtboard.setAttribute("class" , "ghostArtboard");
+                ghostArtboard.setAttribute("id" , "ghostArtboard-"+i);
                 ghostArtboard.style.width = item.width * viewport.zoomFactor;
                 ghostArtboard.style.height = item.height * viewport.zoomFactor;
                 ghostArtboard.style.left = item.globalBounds.x * viewport.zoomFactor;
                 ghostArtboard.style.top = item.globalBounds.y * viewport.zoomFactor;
                 ghostCenter.appendChild(ghostArtboard);
-                ghostCenter.style.left = panel.offsetWidth - (viewport.bounds.x * viewport.zoomFactor);
-                ghostCenter.style.top = panel.offsetHeight - (viewport.bounds.y * viewport.zoomFactor) - panel.offsetHeight - 100;
-            }
+                ghostCenter.style.left = panel.offsetWidth - (viewport.bounds.x * viewport.zoomFactor) + 24;
+                ghostCenter.style.top = panel.offsetHeight - (viewport.bounds.y * viewport.zoomFactor) - panel.offsetHeight - 88;
+            // }
         }
+        // const renditionsFiles = await createRenditions();
+        // for(var i = 0; i < renditionsFiles.length; i++){
+        //     const arrayBuffer = await renditionsFiles[i].read({ format: fs.formats.binary });
+        //     const base64 = base64ArrayBuffer(arrayBuffer);
+        //     const image = document.createElement("img");
+        //     image.setAttribute("src", `data:image/png;base64,${base64}`);
+        //     const ghostArtboard = document.getElementById("ghostArtboard-"+i);
+        //     ghostArtboard.appendChild(image);
+        // }
     }else{
         let ghostCenter = document.getElementById("ghostCenter");
-        while (ghostCenter.firstChild) {
+        while(ghostCenter.firstChild) {
             ghostCenter.removeChild(ghostCenter.firstChild);
         }
     }
@@ -166,6 +178,77 @@ async function initializeDefaultSettings(){
             window.localStorage.setItem(key, default_settings[key]);
         }
     }
+}
+
+async function createRenditions() {
+    const folder = await fs.localFileSystem.getTemporaryFolder();
+    let arr = [];
+    for(var i = 0; i < scenegraph.root.children.length; i++){
+        await folder.createFile(`${scenegraph.root.children.at(i).guid}.png`, { overwrite: true }).then(file => {
+            let obj = {};
+            obj.node = scenegraph.root.children.at(i);
+            obj.outputFile = file;
+            obj.type = "png";
+            obj.background = new Color("#ffffff", 0);
+            obj.scale = 1;
+            arr.push(obj);
+        });
+    }
+    const renditionResults = await application.createRenditions(arr);
+    const renditionsFiles = renditionResults.map(a => a.outputFile);
+    return renditionsFiles;
+}
+
+function base64ArrayBuffer(arrayBuffer) {
+    let base64 = ''
+    const encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+    const bytes = new Uint8Array(arrayBuffer)
+    const byteLength = bytes.byteLength
+    const byteRemainder = byteLength % 3
+    const mainLength = byteLength - byteRemainder
+
+    let a, b, c, d
+    let chunk
+
+    // Main loop deals with bytes in chunks of 3
+    for (var i = 0; i < mainLength; i = i + 3) {
+        // Combine the three bytes into a single integer
+        chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
+
+        // Use bitmasks to extract 6-bit segments from the triplet
+        a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
+        b = (chunk & 258048) >> 12 // 258048   = (2^6 - 1) << 12
+        c = (chunk & 4032) >> 6 // 4032     = (2^6 - 1) << 6
+        d = chunk & 63               // 63       = 2^6 - 1
+
+        // Convert the raw binary segments to the appropriate ASCII encoding
+        base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
+    }
+
+    // Deal with the remaining bytes and padding
+    if (byteRemainder == 1) {
+        chunk = bytes[mainLength]
+
+        a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
+
+        // Set the 4 least significant bits to zero
+        b = (chunk & 3) << 4 // 3   = 2^2 - 1
+
+        base64 += encodings[a] + encodings[b] + '=='
+    } else if (byteRemainder == 2) {
+        chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
+
+        a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
+        b = (chunk & 1008) >> 4 // 1008  = (2^6 - 1) << 4
+
+        // Set the 2 least significant bits to zero
+        c = (chunk & 15) << 2 // 15    = 2^4 - 1
+
+        base64 += encodings[a] + encodings[b] + encodings[c] + '='
+    }
+
+    return base64
 }
 
 async function show(event) {
